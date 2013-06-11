c00d04a4cafe7c1fe3fee2fb70f2aa4f47d48ec3
PrzemyslawMalinowski <boroowa@gmail.com>
2013-06-11 18:52:23 +0200

    zmiany

---
+++ eeprom.h
@@ -0,0 +1,21 @@
+/*
+ * CFile1.c
+ *
+ * Created: 2013-06-11 18:50:12
+ *  Author: Przemo
+ */ 
+
+#include <avr/io.h>
+#include <avr/eeprom.h>
+
+int actual_position = 0;
+
+char* dodajString(char* s)
+{
+	
+}
+
+char* czytajString()
+{
+	
+}
