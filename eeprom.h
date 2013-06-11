ffeb37aaf0b2ed6628da81ab87eb449eed7c3a42
PrzemyslawMalinowski <boroowa@gmail.com>
2013-06-11 18:57:04 +0200

    zmiany

--- eeprom.h
+++ eeprom.h
@@ -12,10 +12,26 @@ int actual_position = 0;
 
 char* dodajString(char* s)
 {
+	int length;
 	
+	char* txt;
+	
+	while(txt != NULL)
+	{
+		length ++;
+		txt ++;
+	}
+	
+	write_eeprom_array(actual_position,s,length);
+	
+	actual_position += 50;
 }
 
-char* czytajString()
+char* czytajString(int start, int length)
 {
 	
+	char* txt;
+	read_eeprom_array(start,txt,length);
+	
+	return txt;
 }
